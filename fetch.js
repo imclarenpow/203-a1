947c9e00d11818a0cb0c4df76c49feeb5b0d34aa
Isaac Powell <powis111@student.otago.ac.nz>
2023-07-31 18:24:20 +1200

    Changes to html css & js (so everything) >> html: fixed the incorrect div id's and classes etc >> css: fixed an issue where the right side of the stats         was being aligned to the left >> js: fixed some issues with incorrect appending
    >>> repository uploaded so i can continue on desktop later tonight

--- fetch.js
+++ fetch.js
@@ -1,46 +1,81 @@
-async function fetcher(){
-    const cardContainer = document.getElementById('cardContainer');
-    try {
-        const response = await fetch('data/nzbird.json');
-        const jsonData = await response.json();
+async function fetcher() {
+  const cardContainer = document.getElementById('cardContainer');
+  try {
+    const response = await fetch('data/nzbird.json');
+    const jsonData = await response.json();
 
-        jsonData.forEach(bird => {
-            const card = createBirdCard(bird);
-            cardContainer.appendChild(card);
-        });
+    jsonData.forEach(bird => {
+      const cardWrapper = document.createElement('div');
+      cardWrapper.classList.add('card-wrapper'); // Add a new class for the wrapper
 
-    }catch (error) {
-        console.error('error when trying to fetch nzbird.json data', error);
-    }
+      const card = createBirdCard(bird);
+      cardWrapper.appendChild(card); // Append the card to the wrapper
+
+      cardContainer.appendChild(cardWrapper); // Append the wrapper to the main container
+    });
+  } catch (error) {
+    console.error('Error when trying to fetch nzbird.json data', error);
+  }
 }
-
-function createBirdCard(bird){
+  
+function createBirdCard(bird) {
     const card = document.createElement('div');
     card.classList.add('card-container');
-
+  
     const image = document.createElement('img');
     image.classList.add('bird-image');
     image.src = bird.photo.source;
     image.alt = bird.english_name;
-
+  
     const name = document.createElement('div');
     name.classList.add('bird-name');
+    name.textContent = bird.primary_name;
+  
+    const type = document.createElement('div');
+    type.classList.add('bird-type');
     type.textContent = bird.scientific_name;
+  
+    const statsList = document.createElement('ul');
+    statsList.classList.add('bird-stats');
+    const otherNames = bird.other_names.slice(0,3).join(', ');
+    const stats = {
+      'Other Names': otherNames,
+      'Family': bird.family,
+      'Size': bird.size.length.value + ' ' + bird.size.length.units,
+      'Weight': bird.size.weight.value + ' ' + bird.size.weight.units,
+      'Status': bird.status,
+      'Order': bird.order
+    };
+  
+    for (const stat in stats) {
+      const listItem = document.createElement('li');
+      const statLabel = document.createElement('span');
+      const statData = document.createElement('span');
+  
+      statLabel.classList.add('stat-label');
+      statLabel.textContent = stat + ':';
+  
+      statData.textContent = stats[stat];
+  
+      listItem.appendChild(statLabel);
+      listItem.appendChild(statData);
+      statsList.appendChild(listItem);
+    }
+  
+    const photoCredit = document.createElement('div');
+    photoCredit.classList.add('bird-credit');
+    photoCredit.textContent = 'Photo Credit: ' + bird.photo.credit;
+  
 
-    const description = document.createElement('div');
-    description.classList.add('bird-description');
-    description.textContent = bird.other_names[0] + ', ' + 'is a bird species belonging to the family ' + bird.family + '. ' + 'It is about ' + bird.size.length.value + ' ' + bird.size.length.units + ' long and weights approximately ' + bird.size.weight.value + ' ' + bird.size.weight.units + '. ' + bird.english_name + ' is classified as "' + bird.status + '" and is found in the order ' + bird.order + '.';
+  card.appendChild(image);
+  card.appendChild(name);
+  card.appendChild(type);
+  card.appendChild(statsList);
+  card.appendChild(photoCredit);
 
-    const photoCred = document.createElement('div');
-    photoCred.classList.add('photo-credit');
-    photoCred.textContent = 'Photo Credit: ' + bird.photo.credit;
-
-    card.appendChild(image);
-    card.appendChild(name);
-    card.appendChild(type);
-    card.appendChild(description);
-    card.appendChild(photoCredit);
-
-    return card;
+  return card;
 }
-fetcher();
+  
+  
+  fetcher();
+  
