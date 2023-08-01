0bcd2b783ac9ac194a6e606587d3c05b0b37b6ae
Isaac <powis111@student.otago.ac.nz>
2023-08-01 20:33:28 +1200

    done for the  night thank you very much

--- fetch.js
+++ fetch.js
@@ -1,3 +1,35 @@
+//fancy scrolling values and stuff so it doesn't feel gross
+let isScrolling = false;
+let lastTimestamp = 0;
+let scrollAmount = 0;
+let decelerationRate = 0.9;
+
+//fancy scrolling logic, basically checks if scrolling and once wheel not being used adds decel
+function scrollHorizontally(timestamp) {
+  if (!isScrolling) return;
+
+  const container = document.getElementById('cardContainer');
+  const deltaTime = timestamp - lastTimestamp;
+  scrollAmount *= decelerationRate;
+  container.scrollLeft += scrollAmount;
+
+  if (Math.abs(scrollAmount) > 0.1) {
+    lastTimestamp = timestamp;
+    requestAnimationFrame(scrollHorizontally);
+  } else {
+    isScrolling = false;
+  }
+}
+//main regular old scrolling logic for horizontal scrolling
+const container = document.getElementById('cardContainer');
+container.addEventListener('wheel', (event) => {
+  event.preventDefault();
+  isScrolling = true;
+  scrollAmount += event.deltaY;
+  lastTimestamp = performance.now();
+  scrollHorizontally(performance.now());
+});
+
 // inital fetch (name)
 fetch(0);
 // makes the page change with the sort button being used
