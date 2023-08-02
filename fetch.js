76acfa8721427e6d4694e3149d09759e3db81e73
Isaac Powell <powis111@student.otago.ac.nz>
2023-08-02 15:29:59 +1200

    Major JS Changes >> moved scrolling logic into a seperate script file >> seperated different logic (including sorting) into different methods >>> this is so that one or other can be loaded at different times >> sortByValue and jsonData are now universal values >>> this is so that these values can be used and modified by different     functions now that I've split up all of the different logic.

--- fetch.js
+++ fetch.js
@@ -1,91 +1,99 @@
-//fancy scrolling values and stuff so it doesn't feel gross
-let isScrolling = false;
-let lastTimestamp = 0;
-let scrollAmount = 0;
-let decelerationRate = 0.9;
+//the overpowering variable that must see all
+let jsonData;
+let sortByValue;
 
-//fancy scrolling logic, basically checks if scrolling and once wheel not being used adds decel
-function scrollHorizontally(timestamp) {
-  if (!isScrolling) return;
-
-  const container = document.getElementById('cardContainer');
-  const deltaTime = timestamp - lastTimestamp;
-  scrollAmount *= decelerationRate;
-  container.scrollLeft += scrollAmount;
-
-  if (Math.abs(scrollAmount) > 0.1) {
-    lastTimestamp = timestamp;
-    requestAnimationFrame(scrollHorizontally);
-  } else {
-    isScrolling = false;
-  }
-}
-//main regular old scrolling logic for horizontal scrolling
-const container = document.getElementById('cardContainer');
-container.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  isScrolling = true;
-  scrollAmount += event.deltaY;
-  lastTimestamp = performance.now();
-  scrollHorizontally(performance.now());
-});
-
-// inital fetch (name)
-fetch(0);
-// makes the page change with the sort button being used
 document.addEventListener('DOMContentLoaded', () => {
+  //method that deals with the checkbox checking, moved because it was taking up space
+  allCheckboxChecker();
+  //initial fetcher, just realised this is important otherwise no birds 
+  //will be seen until sort or filters are used
+  fetchAndSort();
   const sortBySel = document.getElementById('sort');
-  sortBySel.addEventListener('change', sortChange);
-  fetcher(sortBySel.value);
+  const filterUse = document.getElementById('checkbox-group');
+  filterUse.addEventListener('change', (event) =>{
+    const filterValues = checkChange(event);
+    filterLogic(filterValues);
+  });
+  //so that i can use the return value to push into the fetcher.
+  sortBySel.addEventListener('change', (event) => {
+    sortByValue = sortChange(event);
+    fetchAndSort();
+  });
 });
+
+//this is the checkbox logic for the checkboxes
+function checkChange(event) {
+  const filterUse = document.getElementById('checkbox-group');
+  const checkedCheckboxes = getCheckedCheckboxes(filterUse);
+  console.log(checkedCheckboxes);
+  return checkedCheckboxes;
+}
+function getCheckedCheckboxes(container){
+  const checkboxes = container.querySelectorAll('input[type="checkbox"]');
+  const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+  return checkedCheckboxes.map(checkbox => checkbox.value);
+}
+//sort change logic and stuff
 function sortChange(event) {
-  const sortByValue = event.target.value;
-  if (sortByValue === 'name') {
-    fetcher(0);
-  } else if (sortByValue === 'weight') {
-    fetcher(1);
-  } else if (sortByValue === 'length') {
-    fetcher(2);
+  const sortByWord = event.target.value;
+  if (sortByWord === 'name') {
+    return 0;
+  } else if (sortByWord === 'weight') {
+    return 1;
+  } else if (sortByWord === 'length') {
+    return 2;
   }
 }
-
-async function fetcher(sortValue) {
+async function filterLogic(applied){
+  fetcher();
+  jsonData.forEach(bird => {
+    console.log('bird-name');
+  });
+}
+async function fetchAndSort(){
+  await fetcher();
+  sorter(sortByValue);
+}
+async function fetcher() {
   const cardContainer = document.getElementById('cardContainer');
   //remove previous containers made and stuff
   cardContainer.innerHTML = '';
   try {
     const response = await fetch('data/nzbird.json');
-    const jsonData = await response.json();
-  //lets chuck in a couple of sorting statements just to check they
-  //work the way they should SKUX! they do
-  if(sortValue===0){
-    //sort by name (Maori) thanks stackoverflow for this one
-    jsonData.sort(function(a,b){
-      return a.primary_name > b.primary_name ? 1: (a.primary_name === b.primary_name ? 0 : -1);
-    });
-  }else if(sortValue===1){
-  //sort by weight 
-    jsonData.sort(function(a,b){
-      return a.size.weight.value - b.size.weight.value;
-    });
-  }else if(sortValue===2){
-  //sort by length
-    jsonData.sort(function(a,b){
-      return a.size.length.value - b.size.length.value;
-    });
-  }
-    jsonData.forEach(bird => {
-      const cardWrapper = document.createElement('div');
-      cardWrapper.classList.add('card-wrapper');
-      const card = createBirdCard(bird);
-      cardWrapper.appendChild(card);
-      cardContainer.appendChild(cardWrapper);
-    });
+    jsonData = await response.json();
   } catch (error) {
     console.error('Error when trying to fetch nzbird.json data', error);
   }
-}
+  sorter(sortByValue);
   
+}
+
+function sorter(sortValue){
+  if(sortValue===0){
+    //sort by name (Maori) thanks stackoverflow for this one
+      jsonData.sort(function(a,b){
+        return a.primary_name > b.primary_name ? 1: (a.primary_name === b.primary_name ? 0 : -1);
+      });
+    }else if(sortValue===1){
+    //sort by weight 
+      jsonData.sort(function(a,b){
+        return a.size.weight.value - b.size.weight.value;
+      });
+    }else if(sortValue===2){
+    //sort by length
+      jsonData.sort(function(a,b){
+        return a.size.length.value - b.size.length.value;
+      });
+    }
+    // sorter is last so it creates the bird cards etc
+      jsonData.forEach(bird => {
+        const cardWrapper = document.createElement('div');
+        cardWrapper.classList.add('card-wrapper');
+        const card = createBirdCard(bird);
+        cardWrapper.appendChild(card);
+        cardContainer.appendChild(cardWrapper);
+      });
+}
 function createBirdCard(bird) {
     const card = document.createElement('div');
     card.classList.add('card-container');
@@ -142,5 +150,29 @@ function createBirdCard(bird) {
   card.appendChild(photoCredit);
 
   return card;
+}
+
+function allCheckboxChecker(){
+  //logic to make the all check check all boxes, and unselect uncheck all boxes
+  const allCheckbox = document.getElementById('all-check');
+  const checkboxes = document.querySelectorAll('#checkbox-group input[type="checkbox"]');
+  //checks on change for the checkbox on whether or not to check the other boxes
+  allCheckbox.addEventListener('change', () => {
+    checkboxes.forEach(checkbox => checkbox.checked = allCheckbox.checked);
+  });
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      if (!checkbox.checked) {
+        allCheckbox.checked = false;
+      } else {
+        const allUnchecked = Array.from(checkboxes).every(checkbox => !checkbox.checked);
+        allCheckbox.checked = !allUnchecked;
+      }
+      const allChecked = Array.from(checkboxes).every(checkbox => checkbox.checked);
+      if (!checkbox.checked && allCheckbox.checked && !allChecked) {
+        allCheckbox.checked = false;
+      }
+    });
+  });
 }
   
